974af5194f8ca429c06c32748bb75a0c7eb0ea55
rithick-1 <119099540+rithick-1@users.noreply.github.com>
2023-09-23 10:29:47 +0530

    Add files via upload

---
+++ Day4/src/components/Dashboard.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { selectUser } from './redux/UserSlice'
+
+
+
+
+
+const Dashboard = () => {
+ const user=useSelector(selectUser);
+ const email=user.user && user.user.email?user.user.email:'Guest';
+
+    
+  return (
+    <div>
+            <header>
+        <h1>E-Restaurant {email} Dashboard</h1>
+       
+    </header>
+  
+    <aside class="sidebar">
+        <nav>
+            <ul>
+                <br/><br/><br/><br/><br/>
+                <li><Link to="/dash">Dashboard</Link></li>
+                <li><Link to="/order">Orders</Link></li>
+                <li><Link to="/menu">Menu</Link></li>
+                <li><Link to="/desert">Deserts</Link></li>
+                <li><Link to="/setting">Settings</Link></li>
+            </ul>
+        </nav>
+    </aside>
+
+    
+
+    <footer>
+        <div>
+            Copyrights
+        </div>
+            
+            <div class="footer ques">
+               privacy&policy
+            </div>
+        
+        
+    </footer>
+        
+    </div>
+          
+  )
+}
+
+export default Dashboard
